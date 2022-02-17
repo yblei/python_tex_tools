0e77aae426cd56c893b500a62832bf1afe5ec543
Yannik Blei <blei@wingcopter.com>
2022-02-17 10:44:16 +0100

    Meda a proper Module.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pythonTexTools',
+      version='0.1',
+      description='Create latex variables from python code.',
+      author='yblei',
+      packages=['pythonTexTools']
+     )
